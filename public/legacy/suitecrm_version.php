f37e9aaf315fba6a1112396f8bed1dea938d013e
Soft_Solutions4U <projectmanagemant@softsolutions4u.info>
2022-03-29 16:00:16 +0530

    Added all code

---
+++ public/legacy/suitecrm_version.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$suitecrm_version = '8.0.4';
+$suitecrm_legacy = '7.12.5';
+$suitecrm_timestamp = '2022-03-01 12:00:00';
